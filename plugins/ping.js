3720b6fd45ff0cf879b6e8b6a36727538b13c87d
Zenn Suka Coding <azeerh.it@gmail.com>
2026-06-19 17:29:42 +0700

    Create ping.js

---
+++ plugins/ping.js
@@ -0,0 +1,41 @@
+import os from 'os';
+
+export default {
+  command: ['ping'],
+  category: 'main',
+  description: 'Cek status dan kecepatan bot',
+  owner: true,
+
+  async execute({ m }) {
+
+    const start = Date.now();
+
+    const runtime = process.uptime();
+
+    const days = Math.floor(runtime / 86400);
+    const hours = Math.floor(runtime % 86400 / 3600);
+    const minutes = Math.floor(runtime % 3600 / 60);
+    const seconds = Math.floor(runtime % 60);
+
+    const ram = (
+      (process.memoryUsage().heapUsed / 1024 / 1024)
+    ).toFixed(2);
+
+    const platform = os.platform();
+
+    const speed = Date.now() - start;
+
+    m.reply(
+`╭───〔 🏓 PING BOT 〕───⬣
+
+⚡ Kecepatan : ${speed} ms
+⏱ Runtime   : ${days} Hari ${hours} Jam ${minutes} Menit ${seconds} Detik
+💾 RAM       : ${ram} MB
+📱 Platform  : ${platform}
+🟢 Status    : Online
+
+╰────────────⬣`
+    );
+
+  }
+};
